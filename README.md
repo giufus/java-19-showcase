e95dd13d2aba00f2adb61dcc4c50747cbe4511f0
giuseppe.fusacchia <giuseppe.fusacchia@assistdigital.com>
2022-12-13 18:16:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Preview of jdk 19 (virtual threads and others...)
+
+- Run it with `--enable-preview` vm option
+- Gradle >= 7.6 needed ([compatibility matrix here](https://docs.gradle.org/current/userguide/compatibility.html))
---
+++ src/main/java/org/giufus/Main.java
@@ -0,0 +1,29 @@
+package org.giufus;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean vThreads = args.length > 0;
+        System.out.println( "Using vThreads: " + vThreads);
+
+        long start = System.currentTimeMillis();
+
+        Random random = new Random();
+        Runnable runnable = () -> { double i = random.nextDouble(1000) % random.nextDouble(1000);  };
+        for (int i = 0; i < 50000; i++){
+            if (vThreads){
+                Thread.startVirtualThread(runnable);
+            } else {
+                Thread t = new Thread(runnable);
+                t.start();
+            }
+        }
+
+        long finish = System.currentTimeMillis();
+        long timeElapsed = finish - start;
+        System.out.println("Run time: " + timeElapsed);
+
+    }
+}
